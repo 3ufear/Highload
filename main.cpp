35b30d6b59ee62756b72775ce665674233479c0f
Philipp Linev <fill991@rambler.ru>
2014-03-29 13:28:10 +0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//#include <server.hpp>
+#include "iostream"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	if (argc != 4) {
+		cout<<"Usage host port doc_root"<<endl;
+	} else {
+        int a=5;
+
+	}
+	return 1;
+}
---
+++ server.cpp
@@ -0,0 +1,20 @@
+/*
+ * server.cpp
+ *
+ *  Created on: 28.03.2014
+ *      Author: phil
+ */
+
+#include "server.hpp"
+
+server::server() {
+	// TODO Auto-generated constructor stub
+
+}
+
+server:
+
+server::~server() {
+	// TODO Auto-generated destructor stub
+}
+
